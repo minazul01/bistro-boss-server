1863264f9a0f83922b4e583abf9f01bef46eb153
minazul01 <minajul4010@gmail.com>
2025-07-18 01:02:52 +0600

    jsonWebToken , verify token, verify admin and etc work complete

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const express = require("express");
 const app = express();
 require("dotenv").config();
+const jwt = require("jsonwebtoken");
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 
@@ -25,29 +26,127 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const userCollection = client.db("bistroDB").collection("user");
     const menuCollection = client.db("bistroDB").collection("menu");
     const reviewCollection = client.db("bistroDB").collection("review");
     const cardCollection = client.db("bistroDB").collection("cards");
 
+    // JSON WEB-TOKEN SECRET
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+      res.send({ token });
+    });
+
+    // middleware
+    const verifyToken = (req, res, next) => {
+      if (!req.headers.authorization) {
+        return res.status(404).send({ message: "forbidden access" });
+      }
+      const token = req.headers.authorization.split(" ")[1];
+
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(404).send({ message: "forbidden access" });
+        }
+        req.decoded = decoded;
+        next();
+      });
+    };
+
+
+    // admin verify
+    const verifyAdmin = async(req, res, next) => {
+      const email = req.decoded.email;
+      const query = {email: email};
+      const user = await userCollection.findOne(query);
+      const isAdmin = user?.role === "admin";
+      if(!isAdmin){
+         res.status(403).send({message: 'unauthorize access'});
+      };
+      next();
+    };
+
+
+
+    // user related api
+    // create users
+    app.post("/users", async (req, res) => {
+      const data = req.body;
+      // user checking, user already database added or no
+      const query = { email: data.email };
+      const exitingUser = await userCollection.findOne(query);
+      if (exitingUser) {
+        return res.send({ message: "user already added this database" });
+      }
+      const result = await userCollection.insertOne(data);
+      res.send(result);
+    });
+
+    // load users
+    app.get("/users", verifyToken, verifyAdmin, async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
+
+    // admin user checking
+    app.get("/users/admin/:email", verifyToken, verifyAdmin, async (req, res) => {
+      const email = req.params.email;
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ message: "unauthorize access" });
+      }
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      let admin = false;
+      if (user) {
+        admin = user?.role === "admin";
+      }
+      res.send({ admin });
+    });
+
+    // delete users
+    app.delete("/users/:id",verifyToken, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // patch the pacific user to admin
+    app.patch("/users/admin/:id", verifyToken, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDocs = {
+        $set: {
+          role: "admin",
+        },
+      };
+      const result = await userCollection.updateOne(filter, updateDocs);
+      res.send(result);
+    });
+
+    //  load menus or item
     app.get("/menus", async (req, res) => {
       const result = await menuCollection.find().toArray();
       res.send(result);
     });
 
+    // load reviews
     app.get("/reviews", async (req, res) => {
       const result = await reviewCollection.find().toArray();
       res.send(result);
     });
 
-    //  add to card
-
+    //  add to card or load shopping card pacific users
     app.get("/cards", async (req, res) => {
       const email = req.query.email;
       const query = { email: email };
       const result = await cardCollection.find(query).toArray();
       res.send(result);
     });
-
+    // create card
     app.post("/cards", async (req, res) => {
       const data = req.body;
       const result = await cardCollection.insertOne(data);
